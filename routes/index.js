f442d7ada3f9c0faedc0bad946fc37d0ca72a334
Fedenoodt <fedenoodtmolins@gmail.com>
2024-03-12 21:17:05 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,10 @@ var router = express.Router();
 
 const productosController = require("../controllers/productsController");
 
-/* GET users listing. */
-//GET - Retornar datos
 router.get("/", productosController.getAll);
-
-//GET - Retornar datos
 router.get("/:id", productosController.getById);
-
-//POST - Crear
 router.post("/", productosController.create);
-
-//PUT - Actualizar
 router.put("/:id", productosController.update);
-
-//DELETE - Eliminar
 router.delete("/:id", productosController.remove);
 
 module.exports = router;
